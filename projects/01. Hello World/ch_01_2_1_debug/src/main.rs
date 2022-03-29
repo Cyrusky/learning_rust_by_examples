078a9bc9979d15bdb2906569cf3156878c4da457
Bo.Jin <bo.jin@borgor.cn>
2022-03-29 21:00:56 +0800

    done

--- projects/01. Hello World/ch_01_2_1_debug/src/main.rs
+++ projects/01. Hello World/ch_01_2_1_debug/src/main.rs
@@ -1,3 +1,24 @@
+// 推导 `Structure` 的 `fmt::Debug` 实现。
+// `Structure` 是一个包含单个 `i32` 的结构体。
+#[derive(Debug)]
+struct Structure(i32);
+
+// 将 `Structure` 放到结构体 `Deep` 中。然后使 `Deep` 也能够打印。
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
-    println!("Hello, world!");
+    // 使用 `{:?}` 打印和使用 `{}` 类似。
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor = "actor's");
+
+    // `Structure` 也可以打印！
+    println!("Now {:?} will print!", Structure(3));
+
+    // 使用 `derive` 的一个问题是不能控制输出的形式。
+    // 假如我只想展示一个 `7` 怎么办？
+    println!("Now {:#?} will print!", Deep(Structure(7)));
 }
